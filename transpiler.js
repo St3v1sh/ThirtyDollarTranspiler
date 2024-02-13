f24c0717ed5071adc092086b41a79bf7279be478
st3v1sh <st3v1sh@gmail.com>
2024-02-13 00:55:55 -0500

    Status updating added

--- transpiler.js
+++ transpiler.js
@@ -1,5 +1,17 @@
 function transpile() {
-    const input = document.getElementById('input').value;
-    const lines = input.split('\n').map((line) => line.trim()).filter((line) => (line.length > 0 && !line.startsWith(SYMBOLS.COMMENT)));
-    console.log(lines);
+  const input = document.getElementById('input').value;
+  const lines = input.split('\n').map((line) => line.trim()).filter((line) => (line.length > 0 && !line.startsWith(SYMBOLS.COMMENT)));
+
+  const songStartIndex = lines.indexOf(SYMBOLS.SONG.START);
+  if (songStartIndex === -1) {
+    reportError('No song found');
+    return;
   }
+
+  const configLines = lines.slice(0, songStartIndex);
+  const songLines = lines.slice(songStartIndex + 1);
+
+  console.log(configLines, songLines);
+
+  reportOK('Song successfully transpiled');
+}
--- utility.js
+++ utility.js
@@ -1,18 +1,59 @@
 const MOYAI = '🗿';
 
 const SYMBOLS = {
-    COMMENT: '-',
-    CONFIG: {
-        NAME: 'name',
-        BPM: 'bpm',
-        SHARP: 'sharp',
-        FLAT: 'flat',
-        TRANSPOSE: 'transpose',
-    },
-    INSTRUMENTS: {
-        INSTRUMENT: 'inst',
-        SET: 'set',
-        VOLUME: 'volume',
-        PITCH: 'pitch',
-    },
+  COMMENT: '-',
+  CONFIG: {
+    NAME: 'name',
+    BPM: 'bpm',
+    SHARP: 'sharp',
+    FLAT: 'flat',
+    TRANSPOSE: 'transpose',
+  },
+  INSTRUMENTS: {
+    INSTRUMENT: 'inst',
+    SET: 'set',
+    VOLUME: 'volume',
+    PITCH: 'pitch',
+  },
+  SONG: {
+    DELIMITER: ':',
+
+    // Delimiter symbols.
+    START: 'start',
+    INSTRUMENT: 'inst',
+    VOLUME: 'vol',
+    GLOBAL_VOLUME: 'gvol',
+    CLEAR: 'clear',
+    TEMPO: 'tempo',
+
+    // Non-delimiter symbols.
+    DIVIDER: 'div',
+    SEGMENT: 'seg',
+    SEGMENT_START: 'segstart',
+    SEGMENT_END: 'segend',
+  }
+}
+
+const dateFormatter = new Intl.DateTimeFormat('en', {
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+});
+
+function reportError(message) {
+  const output = document.getElementById('status');
+  output.style.color = '#fa4d56';
+  output.innerHTML = `${dateFormatter.format(Date.now())} | Error: ${message}`;
+}
+
+function reportWarning(message) {
+  const output = document.getElementById('status');
+  output.style.color = '#f1c21b';
+  output.innerHTML = `${dateFormatter.format(Date.now())} | Warning: ${message}`;
+}
+
+function reportOK(message) {
+  const output = document.getElementById('status');
+  output.style.color = '#24a148';
+  output.innerHTML = `${dateFormatter.format(Date.now())} | OK: ${message}`;
 }
